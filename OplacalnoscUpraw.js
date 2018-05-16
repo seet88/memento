e4d8f06f3b59d7b7638838fb855b99fdf7d58080
seet88 <cokaco88@gmail.com>
2018-05-16 19:11:29 +0200

    Dodanie listy zabiegow

--- OplacalnoscUpraw.js
+++ OplacalnoscUpraw.js
@@ -4,12 +4,12 @@ UzupelnijKosztDlaWszystkichTabel();
 
 function UzupelnijKosztDlaWszystkichTabel(){
 	var listaStrukturTabel = [
-		{Nazwa: "Arch Siew PROD 2017", NazwaCenyZabiegu: "Cena mieszanki (zl/ha)", NazwaZabiegu: "Mieszanina", NumberZabiegu: "Numer mieszanki", Sezon: "2017", Zrodlo: "Siew"},
-		{Nazwa: "Arch SoR Mieszaniny PROD 2017", NazwaCenyZabiegu: "Cena mieszanki (zl/ha)", NazwaZabiegu: "Mieszanina", NumberZabiegu: "Numer mieszanki", Sezon: "2017", Zrodlo: "SoR"},
-		{Nazwa: "Arch PaPu Nawozenie PROD 2017", NazwaCenyZabiegu: "Cena zabiegu (zl/ha)", NazwaZabiegu: "Nazwa zabiegu", NumberZabiegu: "Numer zabiegu", Sezon: "2017", Zrodlo: "PaPu"},
-		{Nazwa: "PaPu Nawozenie PROD", NazwaCenyZabiegu: "Cena zabiegu (zl/ha)", NazwaZabiegu: "Nazwa zabiegu", NumberZabiegu: "Numer zabiegu", Sezon: "2018", Zrodlo: "PaPu"},
-		{Nazwa: "Siew PROD", NazwaCenyZabiegu: "Cena mieszanki (zl/ha)", NazwaZabiegu: "Mieszanina", NumberZabiegu: "Numer mieszanki", Sezon: "2018", Zrodlo: "Siew"},
-		{Nazwa: "SoR Mieszaniny PROD", NazwaCenyZabiegu: "Cena mieszanki (zl/ha)", NazwaZabiegu: "Mieszanina", NumberZabiegu: "Numer mieszanki", Sezon: "2018", Zrodlo: "SoR"}
+		{Nazwa: "Arch Siew PROD 2017", NazwaCenyZabiegu: "Cena mieszanki (zl/ha)", NazwaZabiegu: "Mieszanina", NumerZabiegu: "Numer mieszanki", Sezon: "2017", Zrodlo: "Siew"},
+		{Nazwa: "Arch SoR Mieszaniny PROD 2017", NazwaCenyZabiegu: "Cena mieszanki (zl/ha)", NazwaZabiegu: "Mieszanina", NumerZabiegu: "Numer mieszanki", Sezon: "2017", Zrodlo: "SoR"},
+		{Nazwa: "Arch PaPu Nawozenie PROD 2017", NazwaCenyZabiegu: "Cena zabiegu (zl/ha)", NazwaZabiegu: "Nazwa zabiegu", NumerZabiegu: "Numer zabiegu", Sezon: "2017", Zrodlo: "PaPu"},
+		{Nazwa: "PaPu Nawozenie PROD", NazwaCenyZabiegu: "Cena zabiegu (zl/ha)", NazwaZabiegu: "Nazwa zabiegu", NumerZabiegu: "Numer zabiegu", Sezon: "2018", Zrodlo: "PaPu"},
+		{Nazwa: "Siew PROD", NazwaCenyZabiegu: "Cena mieszanki (zl/ha)", NazwaZabiegu: "Mieszanina", NumerZabiegu: "Numer mieszanki", Sezon: "2018", Zrodlo: "Siew"},
+		{Nazwa: "SoR Mieszaniny PROD", NazwaCenyZabiegu: "Cena mieszanki (zl/ha)", NazwaZabiegu: "Mieszanina", NumerZabiegu: "Numer mieszanki", Sezon: "2018", Zrodlo: "SoR"}
 	]
 	for(var i in listaStrukturTabel){		
 		UzupelnijKosztDlaWszystkichPol(listaStrukturTabel[i]);
@@ -28,7 +28,8 @@ function UzupelnijKosztDlaWszystkichPol(StrukturaTabeli){
 
 function UzupelnijKosztDlaPola(zabiegi,pole,StrukturaTabeli){		
 	var kosztyZabiegu={
-		koszt_zl_ha: []
+		koszt_zl_ha: [],
+		nazwaZabiegu: []
 		};
 	for(var i in zabiegi){
 		kosztyZabiegu = UzupelnijKosztZabieguDlaPola(zabiegi[i],kosztyZabiegu,pole,StrukturaTabeli);
@@ -44,7 +45,11 @@ function UzupelnijKosztZabieguDlaPola(zabieg,kosztyZabiegu, pole,StrukturaTabeli
 				if(pole===zabiegPole){					
 					var status = zabieg.field("Status");
 					var uprawaZabieg = zabieg.field("Uprawa");
-					var nazwaZabiegu = zabieg.field(StrukturaTabeli.NumberZabiegu)+" "+zabieg.field(StrukturaTabeli.NazwaZabiegu);
+					if(kosztyZabiegu.nazwaZabiegu[status]===undefined || kosztyZabiegu.nazwaZabiegu[status]===null || kosztyZabiegu.nazwaZabiegu[status]===NaN)
+						kosztyZabiegu.nazwaZabiegu[status] ="";
+					
+					kosztyZabiegu.nazwaZabiegu[status] += zabieg.field(StrukturaTabeli.NumerZabiegu)+" "+zabieg.field(StrukturaTabeli.NazwaZabiegu)+"; ";
+					
 					if(kosztyZabiegu.koszt_zl_ha[status]===undefined || kosztyZabiegu.koszt_zl_ha[status]===null || kosztyZabiegu.koszt_zl_ha[status]===NaN)
 						kosztyZabiegu.koszt_zl_ha[status]=0;
 					kosztyZabiegu.koszt_zl_ha[status]+=Math.round(Number(src(zabieg.field(StrukturaTabeli.NazwaCenyZabiegu))));	
@@ -54,19 +59,18 @@ function UzupelnijKosztZabieguDlaPola(zabieg,kosztyZabiegu, pole,StrukturaTabeli
 	if(status===undefined){
 		status=kosztyZabiegu.statusZabiegu;
 		uprawaZabieg=kosztyZabiegu.uprawa;
-		nazwaZabiegu=kosztyZabiegu.nazwaZabiegu;
 	}
 	return {
 		koszt_zl_ha: kosztyZabiegu.koszt_zl_ha,
 		statusZabiegu: status,
 		uprawa: uprawaZabieg,
-		nazwaZabiegu: nazwaZabiegu
+		nazwaZabiegu: kosztyZabiegu.nazwaZabiegu
 		};		
 }
 
 function DodajWpisDoOplacalnoscUpraw(kosztyZabiegu,pole,StrukturaTabeli){
 	for(var status in kosztyZabiegu.koszt_zl_ha){
-		if(kosztyZabiegu.koszt_zl_ha[status]>=0){
+		if(kosztyZabiegu.koszt_zl_ha[status]>0){
 			var kosztZabiegow = libByName("Oplacalnosc upraw PROD"); 
 			var nowyKosztZabiegow = new Object();
 			nowyKosztZabiegow["Koszt (zl/ha)"] = kosztyZabiegu.koszt_zl_ha[status];
@@ -75,9 +79,9 @@ function DodajWpisDoOplacalnoscUpraw(kosztyZabiegu,pole,StrukturaTabeli){
 			nowyKosztZabiegow["Uprawa"] = kosztyZabiegu.uprawa;
 			nowyKosztZabiegow["Status"] = status;
 			nowyKosztZabiegow["Sezon"] = StrukturaTabeli.Sezon;
-			nowyKosztZabiegow["Lista zabiegow"] = kosztyZabiegu.NazwaZabiegu;
+			nowyKosztZabiegow["Lista zabiegow"] = kosztyZabiegu.nazwaZabiegu[status];
 			kosztZabiegow.create(nowyKosztZabiegow);
-			//message("koszt_zl_ha: "+kosztyZabiegu.koszt_zl_ha[status]+" dla pola: "+pole+ " uprawa: "+kosztyZabiegu.uprawa+ " statusZabiegu: "+kosztyZabiegu.statusZabiegu);
+			//message("koszt_zl_ha: "+kosztyZabiegu.koszt_zl_ha[status]+" dla pola: "+pole+ " uprawa: "+kosztyZabiegu.uprawa+ " statusZabiegu: "+status+ " nazwaZabiegu: "+kosztyZabiegu.nazwaZabiegu);
 		}
 	}
 }
