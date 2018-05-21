772d8ee6ae261e9d698936ad334c63c60d7fd3a9
seet88 <cokaco88@gmail.com>
2018-05-21 17:27:44 +0200

    dodawanie wpisow bez elementow

--- UzyteProdukty.js
+++ UzyteProdukty.js
@@ -1,21 +1,21 @@
-UzupelnijKosztDlaWszystkichTabel();
 function UzupelnijKosztDlaWszystkichTabel(){
 	var strukturaElementuSkladnika = {Nazwa: "Nazwa skladnika 1", Wartosc: "Wartosc skl 1"};
 	var strukturaSkladnikuZabiegu = {Nazwa: "Nazwa skladnika 1", Wartosc: "wartość skł. 1 (zł/ha)", Dawka: "Dawka 1", IloscSkladnika: "ils skl 1", element: strukturaElementuSkladnika};
 	var listaStrukturTabel = [
-		//{Nazwa: "Arch Siew PROD 2017", NazwaCenyZabiegu: "Cena mieszanki (zl/ha)", NazwaZabiegu: "Mieszanina", NumerZabiegu: "Numer mieszanki", Sezon: "2017", Zrodlo: "Siew", poleUprawa: "lista", skladnik: strukturaSkladnikuZabiegu, nazwaPolaSkladnika: "Nazwa nasion", iloscSkladnikow: 8},
-		//{Nazwa: "Arch SoR Mieszaniny PROD 2017", NazwaCenyZabiegu: "Cena mieszanki (zl/ha)", NazwaZabiegu: "Mieszanina", NumerZabiegu: "Numer mieszanki", Sezon: "2017", Zrodlo: "SoR", poleUprawa: "lista", skladnik: strukturaSkladnikuZabiegu, nazwaPolaSkladnika: "Nazwa srodka", iloscSkladnikow: 8},
-		//{Nazwa: "Arch PaPu Nawozenie PROD 2017", NazwaCenyZabiegu: "Cena zabiegu (zl/ha)", NazwaZabiegu: "Nazwa zabiegu", NumerZabiegu: "Numer zabiegu", Sezon: "2017", Zrodlo: "PaPu", poleUprawa: "lista", skladnik: strukturaSkladnikuZabiegu, nazwaPolaSkladnika: "Nazwa nawozu", iloscSkladnikow: 1},
+		{Nazwa: "Arch Siew PROD 2017", NazwaCenyZabiegu: "Cena mieszanki (zl/ha)", NazwaZabiegu: "Mieszanina", NumerZabiegu: "Numer mieszanki", Sezon: "2017", Zrodlo: "Siew", poleUprawa: "lista", skladnik: strukturaSkladnikuZabiegu, nazwaPolaSkladnika: "Nazwa nasion", iloscSkladnikow: 8},
+		{Nazwa: "Arch SoR Mieszaniny PROD 2017", NazwaCenyZabiegu: "Cena mieszanki (zl/ha)", NazwaZabiegu: "Mieszanina", NumerZabiegu: "Numer mieszanki", Sezon: "2017", Zrodlo: "SoR", poleUprawa: "lista", skladnik: strukturaSkladnikuZabiegu, nazwaPolaSkladnika: "Nazwa srodka", iloscSkladnikow: 8},
+		{Nazwa: "Arch PaPu Nawozenie PROD 2017", NazwaCenyZabiegu: "Cena zabiegu (zl/ha)", NazwaZabiegu: "Nazwa zabiegu", NumerZabiegu: "Numer zabiegu", Sezon: "2017", Zrodlo: "PaPu", poleUprawa: "lista", skladnik: strukturaSkladnikuZabiegu, nazwaPolaSkladnika: "Nazwa nawozu", iloscSkladnikow: 1},
 		{Nazwa: "PaPu Nawozenie PROD", NazwaCenyZabiegu: "Cena zabiegu (zl/ha)", NazwaZabiegu: "Nazwa zabiegu", NumerZabiegu: "Numer zabiegu", Sezon: "2018", Zrodlo: "PaPu", poleUprawa: "biblioteka", skladnik: strukturaSkladnikuZabiegu, nazwaPolaSkladnika: "Nazwa nawozu", iloscSkladnikow: 1},
-		//{Nazwa: "Siew PROD", NazwaCenyZabiegu: "Cena mieszanki (zl/ha)", NazwaZabiegu: "Mieszanina", NumerZabiegu: "Numer mieszanki", Sezon: "2018", Zrodlo: "Siew", poleUprawa: "biblioteka", skladnik: strukturaSkladnikuZabiegu, nazwaPolaSkladnika: "Nazwa nasion", iloscSkladnikow: 8},
-		//{Nazwa: "SoR Mieszaniny PROD", NazwaCenyZabiegu: "Cena mieszanki (zl/ha)", NazwaZabiegu: "Mieszanina", NumerZabiegu: "Numer mieszanki", Sezon: "2018", Zrodlo: "SoR", poleUprawa: "biblioteka", skladnik: strukturaSkladnikuZabiegu, nazwaPolaSkladnika: "Nazwa srodka", iloscSkladnikow: 8}
+		{Nazwa: "Siew PROD", NazwaCenyZabiegu: "Cena mieszanki (zl/ha)", NazwaZabiegu: "Mieszanina", NumerZabiegu: "Numer mieszanki", Sezon: "2018", Zrodlo: "Siew", poleUprawa: "biblioteka", skladnik: strukturaSkladnikuZabiegu, nazwaPolaSkladnika: "Nazwa nasion", iloscSkladnikow: 8},
+		{Nazwa: "SoR Mieszaniny PROD", NazwaCenyZabiegu: "Cena mieszanki (zl/ha)", NazwaZabiegu: "Mieszanina", NumerZabiegu: "Numer mieszanki", Sezon: "2018", Zrodlo: "SoR", poleUprawa: "biblioteka", skladnik: strukturaSkladnikuZabiegu, nazwaPolaSkladnika: "Nazwa srodka", iloscSkladnikow: 8}
+	// ];
+	// message("to moze potrwac kilka minut");
+	// for(var i in listaStrukturTabel){	
+		// UzupelnijUzyteProduktyDlaTabeli(listaStrukturTabel[i]);
+	// }
+// }
+		{Nazwa: "SoR Mieszaniny PROD", NazwaCenyZabiegu: "Cena mieszanki (zl/ha)", NazwaZabiegu: "Mieszanina", NumerZabiegu: "Numer mieszanki", Sezon: "2018", Zrodlo: "SoR", poleUprawa: "biblioteka", skladnik: strukturaSkladnikuZabiegu, nazwaPolaSkladnika: "Nazwa srodka", iloscSkladnikow: 8}
 	];
-	message("to moze potrwac kilka minut");
-	for(var i in listaStrukturTabel){	
-		UzupelnijUzyteProduktyDlaTabeli(listaStrukturTabel[i]);
-	}
-}
-
 function UzupelnijUzyteProduktyDlaTabeli(StrukturaTabeli){
 	var zabiegi = libByName(StrukturaTabeli.Nazwa).entries();
 	for(var i in zabiegi){	
@@ -68,6 +68,7 @@ function DodajElementSkladnika(StrukturaTabeli,daneSkladnika){
 	var skladnik=daneSkladnika[0];
 	var dawkaSkladnika=daneSkladnika[1];
 	var strukturaElementuSkladnika = StrukturaTabeli.skladnik.element;
+	if(StrukturaTabeli.Sezon==="2017" && StrukturaTabeli.Zrodlo==="Siew") return;
 	for(var i=1; i<=6;i++){
 		if(!isEmpty(skladnik[0].field(strukturaElementuSkladnika.Wartosc.replace("1",String(i))))){
 			var element = new Object();
@@ -77,9 +78,9 @@ function DodajElementSkladnika(StrukturaTabeli,daneSkladnika){
 				element.Nazwa=skladnik[0].field(strukturaElementuSkladnika.Nazwa.replace("1",String(i)));
 			
 			element.IloscElementu=skladnik[0].field(strukturaElementuSkladnika.Wartosc.replace("1",String(i)));
-			element.WartoscElementu=dawkaSkladnika * element.IloscElementu;
-			// element.WartoscElementu=9999;
-			element.Jednostka="kg/ha";
+			if(isNumeric(element.IloscElementu) && isNumeric(dawkaSkladnika))
+				element.WartoscElementu=dawkaSkladnika * element.IloscElementu;
+			element.Jednostka="kg/ha"; //Do zastanowienia sie
 			
 			listaElementowSkladnika.push(element);
 		}	
@@ -95,7 +96,32 @@ function DodajUzyteProduktu(StrukturaTabeli,daneZabiegu){
 		var listaSkladnikow=daneZabiegu.skladniki;
 		for(var j in listaSkladnikow){	
 			var listaElementowSkladnika = listaSkladnikow[j].listaElementowSkladnika;
-			for(var k in listaElementowSkladnika){
+			if(!isNaN(listaElementowSkladnika)){
+				for(var k in listaElementowSkladnika){
+					var uzyteProdukty = libByName("Uzyte produkty PROD"); 
+					var nowyUzytyProdukt = new Object();
+					nowyUzytyProdukt["Koszt (zl/ha)"] = daneZabiegu.koszt_zl_ha;
+					nowyUzytyProdukt["Pole"] = pola[i].field("nazwa");
+					nowyUzytyProdukt["Nazwa"] = StrukturaTabeli.Zrodlo;
+					nowyUzytyProdukt["Uprawa"] = daneZabiegu.uprawa;
+					nowyUzytyProdukt["Status"] = daneZabiegu.status;
+					nowyUzytyProdukt["Sezon"] = StrukturaTabeli.Sezon;
+					nowyUzytyProdukt["Nazwa zabiegu"] = daneZabiegu.numerZabiegu+" "+daneZabiegu.nazwaZabiegu;
+					nowyUzytyProdukt["Nazwa skladnika"] = listaSkladnikow[j].Nazwa[0].field(StrukturaTabeli.nazwaPolaSkladnika)+" >R: "+listaSkladnikow[j].Nazwa[0].field("REF");
+					nowyUzytyProdukt["Dawka"] = listaSkladnikow[j].Dawka;
+					nowyUzytyProdukt["Wartosc skladnika"] = listaSkladnikow[j].Wartosc;
+					nowyUzytyProdukt["Ilosc skladnika"] = listaSkladnikow[j].IloscSkladnika;
+					nowyUzytyProdukt["Obszar rzeczywisty"] = daneZabiegu.obszarRzeczywisty;
+					nowyUzytyProdukt["Cena sr wazona"] = listaSkladnikow[j].Nazwa[0].field("Cena sr wazona (zl/l)");
+					nowyUzytyProdukt["Jednostka"] = listaSkladnikow[j].Nazwa[0].field("jednostka opakowania");
+					nowyUzytyProdukt["Nazwa elementu skladnika"] = listaElementowSkladnika[k].Nazwa;
+					nowyUzytyProdukt["Ilosc elementu skladnika"] = listaElementowSkladnika[k].IloscElementu;
+					nowyUzytyProdukt["Jednostka elementu skladnika"] = listaElementowSkladnika[k].Jednostka;
+					nowyUzytyProdukt["Wartosc elementu skladnika"] = listaElementowSkladnika[k].WartoscElementu;
+					
+					uzyteProdukty.create(nowyUzytyProdukt);
+				}
+			}else{
 				var uzyteProdukty = libByName("Uzyte produkty PROD"); 
 				var nowyUzytyProdukt = new Object();
 				nowyUzytyProdukt["Koszt (zl/ha)"] = daneZabiegu.koszt_zl_ha;
@@ -112,13 +138,19 @@ function DodajUzyteProduktu(StrukturaTabeli,daneZabiegu){
 				nowyUzytyProdukt["Obszar rzeczywisty"] = daneZabiegu.obszarRzeczywisty;
 				nowyUzytyProdukt["Cena sr wazona"] = listaSkladnikow[j].Nazwa[0].field("Cena sr wazona (zl/l)");
 				nowyUzytyProdukt["Jednostka"] = listaSkladnikow[j].Nazwa[0].field("jednostka opakowania");
-				nowyUzytyProdukt["Nazwa elementu skladnika"] = listaElementowSkladnika[k].Nazwa;
-				nowyUzytyProdukt["Ilosc elementu skladnika"] = listaElementowSkladnika[k].IloscElementu;
-				nowyUzytyProdukt["Jednostka elementu skladnika"] = listaElementowSkladnika[k].Jednostka;
-				nowyUzytyProdukt["Wartosc elementu skladnika"] = listaElementowSkladnika[k].WartoscElementu;
 				
 				uzyteProdukty.create(nowyUzytyProdukt);
-			}	
+			}
 		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
