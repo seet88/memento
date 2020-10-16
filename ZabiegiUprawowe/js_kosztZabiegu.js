d0731d13c30cdc18446eb3311906c3de5d85f274
seet88 <cokaco88@gmail.com>
2020-10-16 23:13:11 +0200

    Update js_kosztZabiegu.js
    add price for 1 hour

--- ZabiegiUprawowe/js_kosztZabiegu.js
+++ ZabiegiUprawowe/js_kosztZabiegu.js
@@ -1,11 +1,15 @@
 function obliczKosztZabieguObrokiUprawy(rekord){
-    var obszar = 0;
+    var obszar = 0;  
+    var koszt = 0;        
     if(isEmpty(rekord.field("Obszar rzeczywisty")))
         obszar =rekord.field("Pole")[0].field("obszar");
     else 
         obszar = rekord.field("Obszar rzeczywisty");
     
-    var koszt = obszar * rekord.field("Cena za 1ha");
+    if(isEmpty(rekord.field("Cena za 1ha")))
+        koszt = obszar * rekord.field("Cena za 1 godz");
+    else
+        koszt = obszar * rekord.field("Cena za 1ha");
     koszt = Math.round(koszt);
     return srd(koszt);
 }
